6d7a2f6d7d70b8a11a3934da615e734bc5e3ec03
Matthew Schrader <schradermj11@gmail.com>
2020-05-14 03:21:41 -0500

    Configure android sdk

--- roles/fish/files/fish/config.fish
+++ roles/fish/files/fish/config.fish
@@ -2,4 +2,5 @@ set -x LANG 'en_US.UTF-8'
 set -x EDITOR 'vim'
 set -x VISUAL 'vim'
 set -x PAGER 'most'
-set -x _JAVA_AWT_WM_NONREPARENTING=1
+set -x PATH $PATH $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/tools $HOME/bin
+set -x ANDROID_SDK_ROOT $HOME/Android/Sdk

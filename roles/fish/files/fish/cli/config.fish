1983044154303918d66bf6818ac1526bbb62f91b
Matthew Schrader <SchraderMJ11@gmail.com>
2020-05-19 10:41:12 -0500

    Set pager to less

--- roles/fish/files/fish/cli/config.fish
+++ roles/fish/files/fish/cli/config.fish
@@ -1,7 +1,7 @@
 set -x LANG 'en_US.UTF-8'
 set -x EDITOR 'vim'
 set -x VISUAL 'vim'
-set -x PAGER 'most'
+set -x PAGER 'less'
 set -x PATH $PATH $HOME/Android/Sdk/platform-tools $HOME/Android/Sdk/tools $HOME/bin
 set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
 set -gx SYSTEMD_PAGER 'less'
